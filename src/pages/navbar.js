6f09e727dc115607fdf69bf4eab4c766f1b3c916
ethonwilles <ethonwilles@gmail.com>
2019-10-31 13:54:16 -0600

    asdfasdf

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,11 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const handleMoreClick = e => {
+    const drop = document.querySelector(".dropdown");
+    drop.style = {
+      visibility: "visible"
+    };
+  };
+  const handleClose = () => {
+    const close = document.querySelector(".dropdown");
+    console.log(close);
+    close.style = {
+      visibility: "hidden"
+    };
+  };
   return (
-    <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-      <Link>More</Link>
+    <div className="navPage">
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link onClick={handleMoreClick}>More</Link>
+        <div className="dropdown" style={{ visibility: "hidden" }}>
+          <Link>Windows</Link>
+          <Link>Framing</Link>
+          <Link>Home Building</Link>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      </div>
     </div>
   );
 };
